15399e47152291dd2a374d91d4486faa58558a02
dailyautocare <dailyautocare.dk@gmail.com>
2026-07-17 11:40:24 +0200

    Update priser.js

--- priser.js
+++ priser.js
@@ -16,10 +16,6 @@ fetch("priser.json")
         setPrice("eksteriør_person", data.eksteriør_person);
         setPrice("komplet_person", data.komplet_person);
 
-        setPrice("interiør_stor", data.interiør_stor);
-        setPrice("eksteriør_stor", data.eksteriør_stor);
-        setPrice("komplet_stor", data.komplet_stor);
-
         setPrice("interiør_van", data.interiør_van);
         setPrice("eksteriør_van", data.eksteriør_van);
         setPrice("komplet_van", data.komplet_van);
@@ -31,10 +27,6 @@ fetch("priser.json")
         setPrice("interiørplus_person", data.interiørplus_person);
         setPrice("eksteriørplus_person", data.eksteriørplus_person);
         setPrice("kompletplus_person", data.kompletplus_person);
-
-        setPrice("interiørplus_stor", data.interiørplus_stor);
-        setPrice("eksteriørplus_stor", data.eksteriørplus_stor);
-        setPrice("kompletplus_stor", data.kompletplus_stor);
 
         setPrice("interiørplus_van", data.interiørplus_van);
         setPrice("eksteriørplus_van", data.eksteriørplus_van);
